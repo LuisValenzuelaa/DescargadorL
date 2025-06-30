9a39e18ca3817a990e204ac1bb9d1cad6acbf5c0
L-A-V-2-2 <lav80111@gmail.com>
2025-06-30 15:33:47 -0600

    refactorización de "app.py" y la clase Descargador

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
-from flask import Flask, after_this_request, request, send_file, render_template
+from flask import Flask, request, send_file, render_template
 from descargador import Descargador
-import logging
 import os
-app = Flask(__name__)#name equivale a main si el archivo se ejecuta directamente
-logging.basicConfig(level=logging.INFO)
-@app.route("/")#cuando se vaya al directorio raiz se ejecuta la función index
+app = Flask(__name__)
+@app.route("/")
 def index():
-    return render_template("index.html")#busca en la carpeta templates y carga el archivo
+    return render_template("index.html")
 @app.route("/descargar", methods=["POST"])
 def descargar():
-    url = request.form.get("url") # se obtiene del name del html
+    url = request.form.get("url")
     tipo = request.form.get("tipo")
-    if not url:
-        return "URL no proporcionada", 400 # 400 es solicitud incorrrecta, bad error
     descargador = Descargador(url, tipo)
-    ruta_archivo = descargador.descargar()
-    if not ruta_archivo or not os.path.exists(ruta_archivo): # puede ser None
-        return "Error: No se pudo generar el archivo", 500 # 500 es internal several error
-    return send_file(ruta_archivo, as_attachment = True, download_name = os.path.basename(ruta_archivo))  
+    archivo = descargador.descargar()
+    if not archivo or not os.path.exists(archivo): # puede ser None
+        return "Error: No se pudo generar el archivo", 500
+    return send_file(archivo, as_attachment = True, download_name = os.path.basename(archivo))  
 if __name__ == "__main__":
     app.run(debug = True) 
--- descargador.py
+++ descargador.py
@@ -1,6 +1,5 @@
-from yt_dlp import DownloadError, YoutubeDL #YoutubeDL es la clase principal
+from yt_dlp import DownloadError, YoutubeDL 
 import os 
-import logging
 from uuid import uuid4 #crear ids únicos, evita que archivos se sobrescriban
 class Descargador:
     def __init__(self, url, tipo):
@@ -11,23 +10,15 @@ class Descargador:
         nombre_archivo = str(uuid4()) #función de uuid, crea un id aleatorio, se pasa a string para usarlo como nombre
         formato = "140" if self.tipo == "audio" else "18"
         extension = "m4a" if self.tipo == "audio" else "mp4"
-        ruta_salida = f"{nombre_archivo}.%(ext)s"#%(ext)s es la extensión del archivo
-        opciones = { #diccionario
-            "format": formato,
-            "outtmpl": ruta_salida, #output template
-            "merge_output_format": extension #unir audio y video
-        }
+        opciones = {"format": formato, "outtmpl": f"{nombre_archivo}.%(ext)s", "merge_output_format": extension}
         try:    
-            with YoutubeDL(opciones) as ydl: #de YoutubeDL, crea un objeto
+            with YoutubeDL(opciones) as ydl:
                 info = ydl.extract_info(self.url, download = True)
-                ruta_real = ydl.prepare_filename(info)
-                if not os.path.exists(ruta_real):
-                    logging.error(f"EL archivo no se generó: {ruta_real}")
+                ruta_salida = ydl.prepare_filename(info)
+                if not os.path.exists(ruta_salida):
                     return None
-                return ruta_real
+                return ruta_salida
         except DownloadError as e:
-            logging.error(f"Error al descargar: {e}")
             return None
         except Exception as e:
-            logging.error(f"Error inesperado: {e}")
             return None
