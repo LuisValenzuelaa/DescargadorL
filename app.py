31e00142051d4503e0334b36e483647fa9907b85
L-A-V-2-2 <lav80111@gmail.com>
2025-07-03 17:45:22 -0600

    cambios finales para la versión 1

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from flask import Flask, request, send_file, render_template, after_this_request
-from descargador import Descargador
-import os
+from flask import Flask, request, send_file, render_template, after_this_request #web
+from descargador import Descargador #clase importada
+import os #manejar directorios
 from io import BytesIO
-app = Flask(__name__)
-@app.route("/")
+app = Flask(__name__) #se instancia una app de flask
+@app.route("/") #al ir al directorio raíz ejecutar index()
 def index():
     return render_template("index.html")
-@app.route("/descargar", methods=["POST"])
+@app.route("/descargar", methods=["POST"]) #al ir al directorio "/descargar" ejecutar descargar()
 def descargar():
-    url = request.form.get("url")
-    tipo = request.form.get("tipo")
-    descargador = Descargador(url, tipo)
-    archivo = descargador.descargar()
+    url = request.form.get("url") #obtener la url
+    tipo = request.form.get("tipo") # obtener si es audio o video
+    descargador = Descargador(url, tipo) #instanciar la clase pasándole los atributos
+    archivo = descargador.descargar() #usar el método descargar
     if not archivo or not os.path.exists(archivo): # puede ser None
         return "Error: No se pudo generar el archivo", 500
-    with open(archivo, "rb") as f:
+    with open(archivo, "rb") as f: # rb = read binary, lectura binaria
         contenido = BytesIO(f.read())
-    @after_this_request
+    @after_this_request #eliminar el archivo después de send_file
     def limpiar(respuesta):
         if os.path.exists(archivo):
             os.remove(archivo)
         return respuesta
-    return send_file(contenido, as_attachment = True, download_name = os.path.basename(archivo),mimetype = "application/octet-stream")  
+    return send_file(contenido, as_attachment = True, download_name = os.path.basename(archivo),mimetype = "application/octet-stream") #envía el archivo al navegador del usuario y lo descarga  
 if __name__ == "__main__":
     app.run(debug = True) 
--- descargador.py
+++ descargador.py
@@ -1,24 +1,24 @@
-from yt_dlp import DownloadError, YoutubeDL 
-import os 
+from yt_dlp import DownloadError, YoutubeDL #descargar de Youtube 
+import os #manejar directorios
 from uuid import uuid4 #crear ids únicos, evita que archivos se sobrescriban
 class Descargador:
     def __init__(self, url, tipo):
-        self.url = url
+        self.url = url 
         self.tipo = tipo
         
     def descargar(self):
         nombre_archivo = str(uuid4()) #función de uuid, crea un id aleatorio, se pasa a string para usarlo como nombre
-        formato = "140" if self.tipo == "audio" else "18"
+        formato = "140" if self.tipo == "audio" else "18" #140 es 128 kbps para m4a, 18 es 360p con audio para mp4
         extension = "m4a" if self.tipo == "audio" else "mp4"
-        ruta_para_guardar = os.path.join(os.path.expanduser("~"), "Downloads", f"{nombre_archivo}.%(ext)s")
+        ruta_para_guardar = os.path.join(os.path.expanduser("~"), "Downloads", f"{nombre_archivo}.%(ext)s") #enviar el archivo a la carpeta de descargas por defecto, "%(ext)s" es un placeholder
         opciones = {"format": formato, "outtmpl": ruta_para_guardar, "merge_output_format": extension}
         try:    
-            with YoutubeDL(opciones) as ydl:
-                info = ydl.extract_info(self.url, download = True)
+            with YoutubeDL(opciones) as ydl: #se le pasa el diccionario de opciones para instanciar YoutubeDL
+                info = ydl.extract_info(self.url, download = True) 
                 ruta_salida = ydl.prepare_filename(info)
                 if not os.path.exists(ruta_salida):
                     return None
-                return ruta_salida
+                return ruta_salida #envía la ruta completa para el archivo
         except DownloadError as e:
             return None
         except Exception as e:
